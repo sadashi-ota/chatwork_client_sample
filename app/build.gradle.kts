3b54badca6644bad769b3e859c92799f02c1843a
Sadashi Ota <sadashi.ota.dev@gmail.com>
2019-05-30 02:25:24 +0900

    Convert to kotlin for build.gradle.

---
+++ app/build.gradle.kts
@@ -0,0 +1,49 @@
+import de.mannodermaus.gradle.plugins.junit5.junitPlatform
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+    id("de.mannodermaus.android-junit5")
+}
+
+android {
+    compileSdkVersion(Deps.Versions.compileSdk)
+    defaultConfig {
+        applicationId = "com.sadashi.client.chatwork"
+        minSdkVersion(Deps.Versions.minSdk)
+        targetSdkVersion(Deps.Versions.compileSdk)
+        versionCode = 1
+        versionName = "0.0.1"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+        }
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    sourceSets {
+        getByName("main").java.srcDirs("src/main/kotlin")
+        getByName("test").java.srcDirs("src/test/kotlin")
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    testOptions {
+        junitPlatform {
+            filters {
+                includeEngines("spek2")
+            }
+        }
+    }
+}
+
+dependencies {
+    Deps.libraries.forEach { implementation(it) }
+    Deps.testLibraries.forEach { testImplementation(it) }
+}
