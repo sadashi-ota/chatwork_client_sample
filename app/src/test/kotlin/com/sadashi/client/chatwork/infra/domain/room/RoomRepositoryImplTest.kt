40377a483fd764e531d6bdb51c27427f26a18dd1
Sadashi Ota <sadashi.ota.dev@gmail.com>
2019-10-06 23:25:37 +0900

    Implements unit test for getRoom method.

--- app/src/test/kotlin/com/sadashi/client/chatwork/infra/domain/room/RoomRepositoryImplTest.kt
+++ app/src/test/kotlin/com/sadashi/client/chatwork/infra/domain/room/RoomRepositoryImplTest.kt
@@ -5,6 +5,7 @@ import com.sadashi.client.chatwork.domain.auth.AuthorizeRepository
 import com.sadashi.client.chatwork.domain.auth.AuthorizedToken
 import com.sadashi.client.chatwork.domain.auth.RefreshToken
 import com.sadashi.client.chatwork.domain.rooms.Room
+import com.sadashi.client.chatwork.domain.rooms.RoomId
 import com.sadashi.client.chatwork.infra.api.RoomApiClient
 import com.sadashi.client.chatwork.infra.api.json.RoomResponseJson
 import io.mockk.every
@@ -95,6 +96,61 @@ internal class RoomRepositoryImplTest : Spek({
             }
             it("Failed to get room list") {
                 roomRepository.getRooms().test().await()
+                    .assertNoValues()
+                    .assertError(Throwable::class.java)
+                    .assertNotComplete()
+            }
+        }
+    }
+
+    describe("#getRoom") {
+        val validRoomId = RoomId(1)
+        val invalidRoomId = RoomId(-1)
+        context("When getToken() is succeed") {
+            beforeEach {
+                every { authorizeRepository.getToken() } returns Single.just(authorizedToken)
+            }
+            context("When api is succeed") {
+                beforeEach {
+                    room = mockk()
+                    roomResponseJson = mockk()
+                    every {
+                        apiClient.getRoom(eq(authorizedToken.accessTokenString), validRoomId.value)
+                    } returns Single.just(roomResponseJson)
+
+                    every {
+                        RoomConverter.convertToDomainModel(eq(roomResponseJson))
+                    } returns room
+                }
+                it("Succeed to get room list") {
+                    roomRepository.getRoom(validRoomId).test().await()
+                        .assertValue(room)
+                        .assertNoErrors()
+                        .assertComplete()
+                }
+            }
+            context("When api is failed") {
+                beforeEach {
+                    every {
+                        apiClient.getRoom(any(), invalidRoomId.value)
+                    } returns Single.error(Throwable("Dummy error"))
+                }
+                it("Failed to get room list") {
+                    roomRepository.getRoom(invalidRoomId).test().await()
+                        .assertNoValues()
+                        .assertError(Throwable::class.java)
+                        .assertNotComplete()
+                }
+            }
+        }
+        context("When getToken() is failed") {
+            beforeEach {
+                every {
+                    authorizeRepository.getToken()
+                } returns Single.error(Throwable("Dummy error"))
+            }
+            it("Failed to get room list") {
+                roomRepository.getRoom(validRoomId).test().await()
                     .assertNoValues()
                     .assertError(Throwable::class.java)
                     .assertNotComplete()
